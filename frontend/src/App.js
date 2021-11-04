b6df6861c80d0d9270dabe3206a0fec4c8860dc0
morozov.da <morozov.da@gmail.com>
2021-11-04 18:26:04 +0300

    added menu and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import UserList from './components/user.js'
+import MenuFixed from './components/menu.js'
+import Footer from './components/footer.js'
+import axios from 'axios'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users/')
+            .then(response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            }).catch(error => console.log(error))
+        }
+
+
+    render() {
+        return (
+            <div>
+                <MenuFixed menu={this.state.menu}/>
+                <UserList users={this.state.users}/>
+                <Footer footer={this.state.footer}/>
+            </div>
+        )
+    }
+
+
+}
+
+
+
+export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
