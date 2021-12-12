c6781cd691d04d584dd9b0ea401ffd74c9f76f21
morozov.da <morozov.da@gmail.com>
2021-12-12 17:31:56 +0300

    final build (cors)

--- backend/todo_list/settings.py
+++ backend/todo_list/settings.py
@@ -124,23 +124,23 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
-	}
-}
-
 # DATABASES = {
 # 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql',
-# 		'NAME': 'todo-db',
-# 		'USER': 'todo-user',
-# 		'PASSWORD': '123qweA!',
-# 		'HOST': 'db',
-# 		'PORT': 5432,
+# 		'ENGINE': 'django.db.backends.sqlite3',
+# 		'NAME': BASE_DIR / 'db.sqlite3',
 # 	}
 # }
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'todo-db',
+		'USER': 'todo-user',
+		'PASSWORD': '123qweA!',
+		'HOST': 'db',
+		'PORT': 5432,
+	}
+}
 
 
 # Password validation
